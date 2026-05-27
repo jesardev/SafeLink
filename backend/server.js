ffa221d7138e49864072ff5d85b02080cd58702f
Jesar Dev <jesar.dev@gmail.com>
2026-05-27 02:26:36 -0500

    xd

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,10 @@ app.get("/location", (req, res) => {
 
 });
 
+app.get("/", (req, res) => {
+    res.send("Backend SafeLink funcionando 🚀");
+});
+
 app.listen(3000, () => {
 
     console.log("Servidor corriendo en puerto 3000");
